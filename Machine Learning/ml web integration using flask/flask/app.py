de1c32022a96156ba7a7acc4d68aa5d01e23a90a
diggledoot <39117755+diggledoot@users.noreply.github.com>
2020-07-08 19:43:03 +0800

    push

---
+++ Machine Learning/ml web integration using flask/flask/app.py
@@ -0,0 +1,26 @@
+import flask
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+#import in model
+with open(f"model/bike_model.pkl","rb") as f:
+    model = pickle.load(f)
+
+app = flask.Flask(__name__,template_folder='templates')
+
+@app.route("/",methods=['GET','POST'])
+def main():
+    if flask.request.method=="GET":
+        return(flask.render_template('main.html'))
+    if flask.request.method=="POST":
+        temperature = flask.request.form['temperature']
+        humidity = flask.request.form['humidity']
+        windspeed = flask.request.form['windspeed']
+        input_variables = pd.DataFrame([[temperature, humidity, windspeed]],columns=['temperature', 'humidity', 'windspeed'],dtype=float)
+        pred = model.predict(input_variables)[0]
+        return flask.render_template('main.html',original_input={'Temperature':temperature,'Humidity':humidity,'Windspeed':windspeed},result=pred[0])
+
+if __name__=="__main__":
+    app.run()
